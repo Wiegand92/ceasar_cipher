5f0b0b6732a9750a0f1b27e8656b3ee3c0d90216
wiegand92 <wiegand.alex@gmail.com>
2021-05-02 17:01:38 -0400

    second passing test

--- lib/ceasar_cipher.rb
+++ lib/ceasar_cipher.rb
@@ -1,3 +1,17 @@
 def ceasar_cipher(string, shift_amount)
-  return "c"
+  lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+  uppercase = lowercase.map { |letter| letter.upcase}
+
+  split_string = string.split('')
+  new_letters = split_string.map { |letter|
+    if letter == " "
+      " "
+    elsif lowercase.include?(letter)
+      lowercase[lowercase.index(letter) + shift_amount]
+    else
+      raise StandardError.new "I'm not sure what happened"
+    end
+  }
+
+  return new_letters.join('')
 end
--- spec/ceasar_cipher_spec.rb
+++ spec/ceasar_cipher_spec.rb
@@ -2,10 +2,15 @@ require './lib/ceasar_cipher.rb'
 
 describe "#ceasar_cipher" do
   it "shifts letters by the appropriate amount" do
-    amount = 2
-    initial_letter = "a"
+
     result = ceasar_cipher("a", 2)
 
     expect(result).to eql("c")
+  end
+
+  it "does not shift letters by the wrong amount" do
+    result = ceasar_cipher("a", 3)
+
+    expect(result).to eql("d")
   end
 end
